838a646f30aa682b5c335b4e9381b93355fb5148
Nandini1239 <123763157+Nandini1239@users.noreply.github.com>
2023-09-28 02:34:23 +0530

    Update deployment_for_sentiment_analysis.py

--- deployment_for_sentiment_analysis.py
+++ deployment_for_sentiment_analysis.py
@@ -1,68 +1,19 @@
-
-
+import pandas as pd
 import streamlit as st
-import pickle
-from sklearn.feature_extraction.text import TfidfVectorizer
-from PIL import Image
-import re
-import string
-import nltk
-import  spacy
+import joblib
 
-with open("svm_model.pkl", "rb") as file:
-    model = pickle.load(file)
+# Load your trained model (replace 'your_model.pkl' with the actual path)
+model = joblib.load('your_model.pkl')
 
-with open("tfidf_vectorizer.pkl", "rb") as file:
-    vectorizer = pickle.load(file)
+# Streamlit UI
+st.title('Crop Yield Prediction App')
 
-nltk.download('stopwords')
-stopwords = nltk.corpus.stopwords.words('english')
+# Input for ID
+user_input = st.text_input('Enter ID:', 'Default ID')
+predict_button = st.button('Predict')
 
-def clean_text(text):
-    text = text.lower()
-    return text.strip()
-
-def remove_punctuation(text):
-    punctuation_free = "".join([i for i in text if i not in string.punctuation])
-    return punctuation_free
-
-def tokenization(text):
-    tokens = re.split(' ', text)
-    return tokens
-
-def remove_stopwords(text):
-    output = " ".join(i for i in text if i not in stopwords)
-    return output
-
-def lemmatizer(text):
-    nlp = spacy.load('en_core_web_sm')
-    doc = nlp(text)
-    sent = [token.lemma_ for token in doc if not token.text in set(stopwords)]
-    return ' '.join(sent)
-
-st.title("DIGITAL GREEN CROP YIELD PREDICTION")
-st.markdown("TEAM 2")
-image = Image.open("crop.jpg")
-st.image(image, use_column_width=True)
-
-st.subheader("Enter your ID here:")
-user_input = st.text_area("")
-
-if user_input:
-    user_input = clean_text(user_input)
-    user_input = remove_punctuation(user_input)
-    user_input = user_input.lower()
-    user_input = tokenization(user_input)
-    user_input = remove_stopwords(user_input)
-    user_input = lemmatizer(user_input)
-
-if st.button("Predict"):
-    if predict_button:
-        if user_input:
-            st.header("Crop Yield Prediction:")
-            st.subheader(f"The predicted yield for ID {user_input} is: {X_test} tons/acre")
-        else:
-            st.subheader("Please enter an ID for prediction.")
+if predict_button:
+    if user_input:
         # Retrieve the data for the given ID (replace with your data retrieval code)
         # You may need to load the relevant data from your dataset based on the ID
         # Make sure to preprocess the data to match the format expected by your model
@@ -73,12 +24,10 @@ if st.button("Predict"):
         
         # Use the model to make a crop yield prediction
         # Replace this line with the actual prediction code for your model
-        # prediction = model.predict(processed_data)[0]
+        y_predict = model.predict(X_test)  # Replace X_test with your processed data
         
-        # Display the prediction (replace with appropriate output)
-
-
-
-
-
-
+        # Display the prediction
+        st.header("Crop Yield Prediction:")
+        st.subheader(f"The predicted yield for ID {user_input} is: {y_predict[0]} tons/acre")
+    else:
+        st.subheader("Please enter an ID for prediction.")
