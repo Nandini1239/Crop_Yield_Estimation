6c3b2143709bba3f3cfc62b27601b16f192daec9
Nandini1239 <123763157+Nandini1239@users.noreply.github.com>
2023-09-28 02:49:04 +0530

    Update deployment_for_sentiment_analysis.py

--- deployment_for_sentiment_analysis.py
+++ deployment_for_sentiment_analysis.py
@@ -7,36 +7,11 @@ import string
 import nltk
 import  spacy
 
-with open("crop_yield_rf_model.pkl", "rb") as file:
+with open("svm_model.pkl", "rb") as file:
     model = pickle.load(file)
 
 with open("tfidf_vectorizer.pkl", "rb") as file:
     vectorizer = pickle.load(file)
-
-nltk.download('stopwords')
-stopwords = nltk.corpus.stopwords.words('english')
-
-def clean_text(text):
-    text = text.lower()
-    return text.strip()
-
-def remove_punctuation(text):
-    punctuation_free = "".join([i for i in text if i not in string.punctuation])
-    return punctuation_free
-
-def tokenization(text):
-    tokens = re.split(' ', text)
-    return tokens
-
-def remove_stopwords(text):
-    output = " ".join(i for i in text if i not in stopwords)
-    return output
-
-def lemmatizer(text):
-    nlp = spacy.load('en_core_web_sm')
-    doc = nlp(text)
-    sent = [token.lemma_ for token in doc if not token.text in set(stopwords)]
-    return ' '.join(sent)
 
 st.title("DIGITAL GREEN CROP YIELD PREDICTION")
 st.markdown("TEAM 2")
